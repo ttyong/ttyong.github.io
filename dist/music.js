966fe25bacd5f107ae4bbbfd504a61cb3c1a24e3
ttyong <2364076207@qq.com>
2020-03-19 12:12:32 +0800

    Site updated: 2020-03-19 12:12:28

--- dist/music.js
+++ dist/music.js
@@ -4,8 +4,8 @@ const ap = new APlayer({
     autoplay: false,
 	theme: '#FADFA3',
 	volume: 0.7,
-	lrcType: 3,
-	preload: 'auto',
+	//lrcType: 3,
+	preload: 'none',
     audio: [
       {
         name: "You are my sunshine",
@@ -19,6 +19,20 @@ const ap = new APlayer({
         artist: 'Lady Gaga/Bradley Cooper',
         url: 'http://music.163.com/song/media/outer/url?id=1313096578.mp3',
         cover: 'https://s1.ax1x.com/2020/03/18/8d1EGD.th.jpg',
+		lrc: 'https://github.com/ttyong/lrc/blob/master/Shallow%20-%20Lady%20Gaga%2CBradley%20Cooper.lrc',
+      },
+	  {
+        name: "I'm Gonna Getcha Good",
+        artist: 'Shania Twain',
+        url: 'http://music.163.com/song/media/outer/url?id=476081660.mp3',
+        cover: 'https://imgchr.com/i/8rs9Ld',
+		lrc: 'https://github.com/ttyong/lrc/blob/master/Shallow%20-%20Lady%20Gaga%2CBradley%20Cooper.lrc',
+      },
+	  {
+        name: "Fire",
+        artist: 'Gavin Degraw',
+        url: 'http://music.163.com/song/media/outer/url?id=28923579.mp3',
+        cover: 'https://s1.ax1x.com/2020/03/19/8rsH1S.jpg',
 		lrc: 'https://github.com/ttyong/lrc/blob/master/Shallow%20-%20Lady%20Gaga%2CBradley%20Cooper.lrc',
       },
     ]
